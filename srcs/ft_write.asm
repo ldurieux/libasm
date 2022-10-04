5eb8e0ae888fb3c8e658e269ece3150e40de340e
Louis Durieux <louis.durieux99@gmail.com>
2022-10-04 17:38:16 +0200

    fix errno in ft_write

--- srcs/ft_write.asm
+++ srcs/ft_write.asm
@@ -1,4 +1,8 @@
 global ft_write
+; linux
+extern __errno_location
+; OSX
+; extern ___error
 
 section .data
 section .note.GNU-stack
@@ -9,4 +13,20 @@ ft_write:
 	mov ebx, edi
 	mov rcx, rsi
 	int 80h
+	; linux
+	cmp rax, -4095
+	jae .error
+	; OSX
+	; jc .error
+	ret
+.error:
+	mov rbx, rax
+	; linux
+	xor rbx, -1	; rbx *= -1
+	inc rbx
+	call __errno_location
+	; OSX
+	; call ___error
+	mov [rax], rbx
+	mov rax, -1
 	ret
